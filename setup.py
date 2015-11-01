2697340aee3fce5867a6dd9a411d31d23d9391ee
Samantha Marshall <me@samdmarshall.com>
2015-10-31 21:40:38 -0400

    [Setup] * removing .clang-format * adding setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='pyXcode',
+    version='0.1',
+    description='Xcode Build System Library',
+    url='https://github.com/samdmarshall/pyXcode',
+    author='Samantha Marshall',
+    author_email='me@samdmarshall.com',
+    license='BSD 3-Clause',
+    package_data = {
+        
+    },
+    packages=[
+        
+    ],
+    zip_safe=False
+)
